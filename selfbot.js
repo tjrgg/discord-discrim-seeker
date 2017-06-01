7071b2483c8613535b8e160074a15f58dcfa3227
Tyler0130 <rocket0191@gmail.com>
2017-06-01 08:33:04 +0000

    Added some error handlers

--- selfbot.js
+++ selfbot.js
@@ -5,24 +5,37 @@ const config = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true });
 
-const discrimSeeker = () => {
+const discrimSeeker = async () => {
     let users = client.users.filter(u => u.discriminator === client.user.discriminator);
     if (users.size < 1) {
         console.error("A user wasn't found with your current discriminator, which doesn't allow this selfbot to work. (Join more servers.)");
         process.exit();
     }
     let user = users.random();
-    client.user.setUsername(user.username, config.password);
-    if (config.discriminator.indexOf(client.user.discriminator) > -1) {
-        console.log("One of your choosen discriminators was selected. Will set your username in 5 minutes. DO NOT EXIT THIS PROGRAM.");
-        client.setTimeout(() => {
-            console.log("Setting your username...");
-            client.user.setUsername(config.username, config.password);
-            console.log("Username set. Exiting...");
-            process.exit();
-        }, 300000);
+    try {
+        await client.user.setUsername(user.username, config.password);
+        if (config.discriminator.indexOf(client.user.discriminator) > -1) {
+            console.log("One of your choosen discriminators was selected. Will set your username in 5 minutes. DO NOT EXIT THIS PROGRAM.");
+            client.setTimeout(() => {
+                try {
+                    console.log("Setting your username...");
+                    await client.user.setUsername(config.username, config.password);
+                    console.log("Username set. Exiting...");
+                    process.exit();
+                }
+                catch (e) {
+                    console.error("Your username failed to reset to your preferred username. You'll have to manually reset your username.");
+                    process.exit();
+                }
+            }, 300000);
+        }
+        else return console.log("The selected discriminator was not one of your preferred discriminators. Will reset your username in 30 minutes.");
     }
-    else return console.log("The selected discriminator was not one of your preferred discriminators. Will reset your username in 30 minutes.");
+    catch (e) {
+        console.error("Your username failed to set.");
+        console.error(e);
+    }
+    
 };
 
 client.on("ready", () => {
