b2b8e0f08835054c145edbdf5a6f0012eb874823
Tyler0130 <rocket0191@gmail.com>
2017-06-01 07:30:57 +0000

    Fixed bug

--- selfbot.js
+++ selfbot.js
@@ -1,4 +1,4 @@
-const config = require("config.json");
+const config = require("./config.json");
 
 const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true });
